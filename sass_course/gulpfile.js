852fe1ebf50a6a1f718b2ca4a5630b33a05d1731
Leonardo Rivaldo <leonardo.rivaldo@gmail.com>
2018-06-09 21:20:38 -0300

    continuando

--- sass_course/gulpfile.js
+++ sass_course/gulpfile.js
@@ -5,8 +5,8 @@ var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    // .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
 });
 
